6a60e1f95fd85185440d9efa7c7d9bd3ffa90cfa
joaquin cuneo <joaquincuneo@hotmail.com>
2018-11-03 16:45:23 +0200

    Taxes final

---
+++ Dolar/Taxes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolarRate = 3.89, tax = 0;
+
+            Console.WriteLine("Please enter your gross salary in shekels");
+            double salary = double.Parse(Console.ReadLine());
+
+            if (salary > 0 && salary <= 2000)
+            {
+                tax = 0;
+            }
+
+            if (salary > 2000 && salary <= 4770)
+            {
+                tax += 0.1 * (salary);
+            }
+
+            if (salary > 4770 && salary <= 10000)
+            {
+                tax += 0.3 * (salary);
+            }
+
+            if (salary > 10000)
+            {
+                tax += 0.5 * (salary);
+            }
+
+            Console.WriteLine("Gross Salary: {0}", salary);
+            Console.WriteLine("Tax to pay: {0}", tax);
+            Console.WriteLine("Net Salary: {0}", salary - tax);
+            Console.WriteLine("Net Salary in dolars: ${0}", Math.Round(salary / dolarRate, 2));
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
+
